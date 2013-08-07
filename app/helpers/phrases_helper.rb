ebf15baa4b70ff0e1fa84b426a5b3e93d45df5d9
YO <aftl84@gmail.com>
2013-08-07 10:39:31 -0400

    in landing page feed, properly showing mintues or min , etc. depending on posting time

--- app/helpers/phrases_helper.rb
+++ app/helpers/phrases_helper.rb
@@ -29,16 +29,38 @@ module PhrasesHelper
 			b = (a / 24).to_i
 			return b.to_s + day_or_days(b)			
 		else
-			return a.round(0).to_s + " hours and " + ((a-(a.to_i))*60).to_i.to_s + " minutes ago"
+			if a.round(0) < 1
+				m = ((a-(a.to_i))*60).to_i
+				return m.to_s + minutes_or_min(m)
+			else
+				hour_only = a.round(0)
+				return hour_only.to_s + hour_or_hours(hour_only)
+			end
 		end
 	end
 
 	def day_or_days(b)
-		if b < 1
+		if b > 1
 			return " days ago"
 		else
 			return " day ago"
 		end
+	end
+
+	def minutes_or_min(m)
+		if m  < 1
+			return " minute ago"
+		else
+			return " minutes ago"
+		end
+	end
+
+	def hour_or_hours(hour_only)
+		if hour_only < 1
+			return " hour ago"
+		else
+			return " hours ago"
+		end	
 	end
 
 	def pt_link(pt)
